e045600d180e21a97f4301518bdbc30031da004a
Vindi <vindisovia@gmail.com>
2021-09-14 13:55:30 +0700

    first commit

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about(){
+        echo "Vindi Sovia Anggita <br>";
+        echo "2031710019";
+    }
+
+}
